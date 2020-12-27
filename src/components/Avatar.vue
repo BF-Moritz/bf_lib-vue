cf6fb3427594063bbdcec1b528faebb287cc4e47
BF-Moritz <bf_moritz@yahoo.de>
2020-12-28 00:26:07 +0100

    first commit

---
+++ src/components/Avatar.vue
@@ -0,0 +1,14 @@
+<template>
+	<div class="avatar">
+		<img :src="imgSrc" :alt="imgAlt" />
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Avatar',
+	props: {}
+};
+</script>
+
+<style lang="stylus" scoped></style>
